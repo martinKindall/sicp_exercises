b474fc80b4a76f9ed2393aaa5df8fb0d227b22a5
Martin Cornejo <mart256@gmail.com>
2017-09-26 19:11:18 -0300

    ex_1_37 finished

---
+++ ex_1_37.scm
@@ -0,0 +1,25 @@
+(define (cont-frac n d k)
+	(define (recursive i)
+		(if (= i k)
+		    (/ (n k) (d k))
+		    (/ (n i) (+ (d i) (recursive (1+ i))))
+		)
+	)
+	(recursive 1)
+)
+
+(define (cont-frac-iter n d k)
+	(define (iter i res)
+		(if (= i 0)
+		    res
+		    (iter (-1+ i) (/ (n i) (+ (d i) res)))
+		)
+	)
+	(iter (-1+ k) (/ (n k) (d k)))
+)
+
+
+(let ((phi (cont-frac-iter (lambda (x) 1.0) (lambda (x) 1.0) 12)))
+	(/ 1 phi)
+)
+

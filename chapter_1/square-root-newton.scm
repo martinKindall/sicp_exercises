d89f9aba7d1109158dc8546cd1f92c496fcf62fc
Martin Cornejo <mart256@gmail.com>
2017-10-17 23:19:09 -0300

    added sqrt procedures

---
+++ chapter_1/square-root-newton.scm
@@ -0,0 +1,32 @@
+(define (iterative-improvement good-enough? improve-guess)
+  	(define (iter guess)
+		(if (good-enough? guess)
+	    	guess
+	    	(iter (improve-guess guess))
+		)
+	)
+	iter
+)
+
+(define tolerance 0.0001)
+
+(define (fixed-point f first-guess)
+	((iterative-improvement (lambda (y) (< (abs (- (f y) y)) tolerance)) f) first-guess)
+)
+
+(define dx tolerance)
+
+(define (deriv g)
+	(lambda (x) (/ (- (g (+ x dx)) (g x)) dx))
+)
+
+(define (newton-transform g)
+	(lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newton-method g guess)
+	(fixed-point (newton-transform g) guess)
+)
+
+(define (sqrt x)
+	(newton-method (lambda (y) (- (square y) x)) 1.0)
+)

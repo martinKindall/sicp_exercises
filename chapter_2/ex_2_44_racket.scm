a4243ec661df136aba4ff2841d22523d5be15417
Martin Cornejo <mart256@gmail.com>
2017-10-29 12:38:58 -0300

    ex_2_44 scheme and racket version

---
+++ chapter_2/ex_2_44_racket.scm
@@ -0,0 +1,36 @@
+#lang racket
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
+(define (right-split painter n)
+  	(if (= n 0)
+  	    painter
+  	    (let ((smaller (right-split painter (- n 1))))
+  	    	(beside painter (bellow smaller smaller))
+  	    )
+  	)
+)
+
+(define (up-split painter n)
+  	(if (= n 0)
+  	    painter
+  	    (let ((smaller (up-split painter (- n 1))))
+  	    	(bellow painter (beside smaller smaller))
+  	    )
+  	)
+)
+
+(define (corner-split painter n)
+  	(if (= n 0)
+  	    painter
+  	    (let ((up (up-split painter (- n 1)))
+  	    	   (right (right-split painter (- n 1)))
+  	    	 )
+  	    	(let ((top-left (beside up up))
+  	    		   (bottom-right (bellow right right))
+  	    		   (corner (corner-split painter (- n 1)))
+  	    	     )
+  	    		(beside (below painter top-left) (below bottom-right corner))
+  	    	)
+  	    )
+  	)
+)

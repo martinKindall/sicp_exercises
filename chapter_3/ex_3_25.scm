0f57511fedd13f24142a028cfb35d8e214fc0a8e
Martin Cornejo <mart256@gmail.com>
2018-01-22 22:48:49 -0300

    ex_3_25 bad imlementation of insert!

--- chapter_3/ex_3_25.scm
+++ chapter_3/ex_3_25.scm
@@ -1,8 +1,8 @@
-(define (make-table same-key?)
+(define (make-table)
 	
 	(define (assoc key records)
 	  	(cond ((null? records) false)
-	  	      ((same-key? key (caar records)) (car records))
+	  	      ((equal? key (caar records)) (car records))
 	  	      (else (assoc key (cdr records)))
 	  	)
 	)
@@ -10,32 +10,49 @@
 	(let ((local-table (list '*table*)))
 		; private methods
 
-		(define (lookup key-1 key-2)
-			(let ((subtable (assoc key-1 (cdr local-table))))
-				(if subtable
-				    (let ((record (assoc key-2 (cdr subtable))))
-				    	(if record
-				    	    (cdr record)
-				    	    false
-				    	)
-				    )
-				    false
+		(define (lookup . list-keys)
+			(define (iter current-keys current-table)
+				(let ((subtable (assoc (car current-keys) (cdr current-table))))
+					(debug subtable)
+					(if subtable
+					    (if (null? (cdr current-keys))
+					        (cdr subtable)
+					        (iter (cdr current-keys) subtable)
+					    )
+					    false
+					)
 				)
 			)
+			(iter list-keys local-table)
 		)
 
-		(define (insert! key-1 key-2 value)
-		  	(let ((subtable (assoc key-1 (cdr local-table))))
-		  		(if subtable
-		  		    (let ((record (assoc key-2 (cdr subtable))))
-		  		    	(if record
-		  		    	    (set-cdr! record value)
-		  		    	    (set-cdr! subtable (cons (cons key-2 value) (cdr subtable)))
-		  		    	)
-		  		    )
-		  		    (set-cdr! local-table (cons (list key-1 (cons key-2 value)) (cdr local-table)))
+		(define (insert! value . list-keys)
+
+		  	(define (form-list list-keys value)
+		  		(let ((first-key (car list-keys)))
+		  			(cond ((null? (cdr list-keys)) (cons first-key value))
+		  			      (else (list first-key (form-list (cdr list-keys) value)))
+		  			)
 		  		)
 		  	)
+
+		  	(define (iter current-keys current-table)
+				(let ((subtable (assoc (car current-keys) (cdr current-table))))
+					(debug subtable)
+					(if subtable
+					    (if (null? (cdr current-keys))
+					        (set-cdr! subtable value)
+						    (iter (cdr current-keys) subtable)
+					    )
+					    (if (null? (cdr current-keys))
+					        (set-cdr! current-table (cons (cons (car current-keys) value) (cdr current-table)))
+						    (set-cdr! current-table (cons (form-list current-keys value) (cdr current-table)))
+					    )
+					)
+				)
+			)
+			(iter list-keys local-table)
+
 		  	'ok
 		)
 
@@ -44,6 +61,7 @@
 		(define (dispatch m)
 		  	(cond ((eq? m 'lookup-proc) lookup)
 		  	      ((eq? m 'insert-proc!) insert!)
+		  	      ((eq? m 'view) local-table)
 		  	      (else (error "Unknown operation: TABLE" m))
 		  	)
 		)
@@ -51,3 +69,16 @@
 		dispatch
 	)
 )
+
+(define (debug x)
+	(newline)
+	(display "----- ")
+	(display x)
+	(display " -----")
+	(newline)
+)
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+(define view-table (operation-table 'view))

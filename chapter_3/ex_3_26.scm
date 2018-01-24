4f44b357c72377a55b005f35d2ff31dcb20ba55c
Martin Cornejo <mart256@gmail.com>
2018-01-24 20:45:32 -0300

    ex_3_26 testing lookup with binary tree

--- chapter_3/ex_3_26.scm
+++ chapter_3/ex_3_26.scm
@@ -1,23 +1,95 @@
+(define nil '())
+
+(define (entry tree)
+  	(car tree)
+)
+
+(define (left-branch tree)
+  	(cadr tree)
+)
+
+(define (right-branch tree)
+  	(caddr tree)
+)
+
+(define (make-tree entry left right)
+  	(list entry left right)
+)
+
+;---------- abstraction barrier ----------
+
+(define (make-record identifier data)
+  	(list identifier data)
+)
+
+(define (key record)
+  	(car record)
+)
+
+(define (record-value record)
+  	(cadr record)
+)
+
+;---------- abstraction barrier ----------
+
+(define (tree-to-list tree)
+  	(define (copy-to-list set result)
+  	  	(if (null? set)
+  	  	    result
+  	  	    (copy-to-list (left-branch set) (cons (entry set) (copy-to-list (right-branch set) result)))
+  	  	)
+  	)
+  	(copy-to-list tree nil)
+)
+
+(define (list-to-tree elements)
+  	(define (partial-tree elts n)
+  	  	(if (= n 0)
+  	  	    (cons nil elts)
+  	  	    (let ((left-size (quotient (-1+ n) 2)))
+  	  	    	(let ((left-result (partial-tree elts left-size)))
+  	  	    		(let ((left-tree (car left-result))
+  	  	    			   (non-left-elts (cdr left-result))
+  	  	    			   (right-size (- n (1+ left-size)))
+  	  	    		     )
+  	  	    			(let ((this-entry (car non-left-elts))
+  	  	    				   (right-result (partial-tree (cdr non-left-elts) right-size))
+  	  	    			     )
+  	  	    				(let ((right-tree (car right-result))
+  	  	    					   (remaining-elts (cdr right-result))
+  	  	    				     )
+  	  	    					(cons (make-tree this-entry left-tree right-tree) remaining-elts)
+  	  	    				)
+  	  	    			)
+  	  	    		)
+  	  	    	)
+  	  	    )
+  	  	)
+  	)
+  	(car (partial-tree elements (length elements)))
+)
+
+(define (lookup-tree given-key tree)
+	(cond ((null? tree) false)
+		  ((= given-key (key (entry tree))) (entry tree))
+	      ((< given-key (key (entry tree))) (lookup-tree given-key (left-branch tree)))
+	      ((> given-key (key (entry tree))) (lookup-tree given-key (right-branch tree)))
+	)
+)
+
 (define (make-table)
 	
-	(define (assoc key records)
-	  	(cond ((null? records) false)
-	  	      ((equal? key (caar records)) (car records))
-	  	      (else (assoc key (cdr records)))
-	  	)
-	)
-
-	(let ((local-table (list '*table*)))
+	(let ((local-table (list-to-tree '((1 martin) (2 george) (3 samuel) (4 john) (5 tom)))))
 		; private methods
 
 		(define (lookup . list-keys)
 			(define (iter current-keys current-table)
-				(let ((subtable (assoc (car current-keys) (cdr current-table))))
+				(let ((subtable (lookup-tree (car current-keys) current-table)))
 					;(debug subtable)
 					(if subtable
 					    (if (null? (cdr current-keys))
-					        (cdr subtable)
-					        (iter (cdr current-keys) subtable)
+					        (record-value subtable)
+					        (iter (cdr current-keys) (record-value subtable))
 					    )
 					    false
 					)
@@ -83,12 +155,12 @@
 (define put (operation-table 'insert-proc!))
 (define view-table (operation-table 'view))
 
-(put 'tamarugo 'arboles 'chile)
-(put 'mango 'arboles 'peru)
-(put 'banano 'arboles 'colombia)
+;(put 'tamarugo 'arboles 'chile)
+;(put 'mango 'arboles 'peru)
+;(put 'banano 'arboles 'colombia)
 
-(put 'sqrt 'math 'functions 'square-root)
-(put 50 'math 'operations '-)
-(put 43 'math 'operations '+)
+;(put 'sqrt 'math 'functions 'square-root)
+;(put 50 'math 'operations '-)
+;(put 43 'math 'operations '+)
 
-view-table
+;view-table

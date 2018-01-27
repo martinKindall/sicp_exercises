92e1153e43dd550e8fecb4a8a696a8afb3f7f74e
Martin Cornejo <mart256@gmail.com>
2018-01-27 14:44:03 -0300

    ex_3_29 finished

---
+++ chapter_3/ex_3_29.scm
@@ -0,0 +1,10 @@
+(define (or-gate a1 a2 output)
+	(let ((b1 (make-wire)) (b2 (make-wire)) (b3 (make-wire)))
+		(inverter a1 b1)
+		(inverter a2 b2)
+		(and-gate b1 b2 b3)
+		(inverter b3 output)
+	)
+)
+
+; the total time is 2*inverter-delay + 1*and-gate-delay

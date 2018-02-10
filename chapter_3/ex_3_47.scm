3563c14ebff6278c0f93bca7bbf0db7d65f6b750
martinKindall <martin@rflex.cl>
2018-02-10 16:01:12 -0300

    ex_3_47 a correct answer to b)

--- chapter_3/ex_3_47.scm
+++ chapter_3/ex_3_47.scm
@@ -11,12 +11,16 @@
   	(set-car! cell false))
 
 (define (test-and-set! cell)
-  	(if (car cell)
-  	    true
-  	    (begin 
-  	    	(set-car! cell true)
-  	    	false
-  	    )))
+    (without-interrupts
+        (lambda () 
+          	(if (car cell)
+          	    true
+          	    (begin 
+          	    	(set-car! cell true)
+          	    	false
+          	    )))))
+
+; implementation b, inefficient compared to the next solution, because of the loops on the cells list)
 
 (define (make-semaphore n)
 	(let ((cells (make-list n '(false))))
@@ -33,6 +37,32 @@
   	      (else false)))
 
 (define (clear-many! cells)
-  	(cond ((null? cells) (error "Releasing an empty mutex"))
-  	      ((car cells) (clear! (car cells)))
-  	      (else (clear-many! (cdr cells)))))
+    ; here without-interrupts is necessary in order to not clear the same cell by two or more concurrent processes
+    (without-interrupts
+        (lambda () 
+          	(cond ((null? cells) (error "Releasing an empty mutex"))
+          	      ((car cells) (clear! (car cells)))
+          	      (else (clear-many! (cdr cells))))))
+
+
+; implementation b-2 (from leafac, schemewiki answers))
+
+(define (make-semaphore maximum-clients) 
+   (let ((access-mutex (list false)) 
+         (clients 0)) 
+     (define (the-semaphore message) 
+       (cond ((eq? message 'acquire) 
+              (if (test-and-set! access-mutex) 
+                  (the-semaphore 'acquire)) 
+              (cond ((> clients maximum-clients) 
+                     (clear! access-mutex) 
+                     (the-semaphore 'acquire)) 
+                    (else 
+                     (set! clients (+ clients 1)) 
+                     (clear! access-mutex)))) 
+             ((eq? message 'release) 
+              (if (test-and-set! access-mutex) 
+                  (the-semaphore 'release)) 
+              (set! clients (- clients 1)) 
+              (clear! access-mutex)))) 
+     the-semaphore))

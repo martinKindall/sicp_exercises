48feedda31b1f61e2a656135fa04e60bb25df72c
martinKindall <martin@rflex.cl>
2018-02-14 22:01:46 -0300

    ex_3_59 finished

--- chapter_3/ex_3_59.scm
+++ chapter_3/ex_3_59.scm
@@ -1,3 +1,5 @@
+; part a)
+
 (define (mul-streams s1 s2)
   	(stream-map * s1 s2))
 
@@ -13,5 +15,10 @@
 
 (define fractions (div-streams ones integers))   ; 1, 1/2, 1/3, 1/4, ...
 
-(define (integrate-series stream constant)
-  	(cons-stream constant (mul-streams stream fractions)))
+(define (integrate-series stream)
+  	(mul-streams stream fractions))
+
+; part b)
+
+(define cosine-series (cons-stream 1 (stream-map (lambda (elem) (* elem -1)) (integrate-series sine-series))))
+(define sine-series (cons-stream 0 (integrate-series cosine-series)))
